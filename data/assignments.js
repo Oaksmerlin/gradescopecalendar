2fdd475adde7faf1f1184413d2ab54cf7932136f
planit <planit@tanjeffreyz.github.io>
2024-08-28 19:37:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,20 @@ assignments = {
       "dueDate": "2024-08-28T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4750140/submissions/262496249",
       "submitted": true
+    },
+    {
+      "title": "HW05W",
+      "course": "MA.26600.116",
+      "dueDate": "2024-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/826425",
+      "submitted": false
+    },
+    {
+      "title": "HW06W",
+      "course": "MA.26600.116",
+      "dueDate": "2024-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/826425",
+      "submitted": false
     }
   ],
   "wl.202510.AAE.25100.002": [
