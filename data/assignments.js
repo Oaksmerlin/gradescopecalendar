0192060ca4c18b1c5bcd9ccedb3082bc58e8df4b
planit <planit@tanjeffreyz.github.io>
2024-09-03 19:37:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -50,6 +50,13 @@ assignments = {
       "dueDate": "2024-09-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4809376/submissions/263649714",
       "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "wl.202510.AAE.25100.002",
+      "dueDate": "2024-09-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/826063",
+      "submitted": false
     }
   ]
 };
