3313b3fba8f30c3e20fa1a0a9bd8eb7edea49273
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:10:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,210 +3,210 @@ assignments = {
     {
       "title": "HW02W",
       "course": "MA.26600.116",
-      "dueDate": "2024-08-28T23:59:00",
+      "dueDate": "2025-08-28T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4750138/submissions/261873684",
       "submitted": true
     },
     {
       "title": "HW03W",
       "course": "MA.26600.116",
-      "dueDate": "2024-08-28T23:59:00",
+      "dueDate": "2025-08-28T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4750139/submissions/262200387",
       "submitted": true
     },
     {
       "title": "HW04W",
       "course": "MA.26600.116",
-      "dueDate": "2024-08-28T23:59:00",
+      "dueDate": "2025-08-28T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4750140/submissions/262496249",
       "submitted": true
     },
     {
       "title": "HW05W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-04T23:59:00",
+      "dueDate": "2025-09-04T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4840923/submissions/263496160",
       "submitted": true
     },
     {
       "title": "HW06W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-04T23:59:00",
+      "dueDate": "2025-09-04T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4840999/submissions/263514228",
       "submitted": true
     },
     {
       "title": "HW08W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-11T23:59:00",
+      "dueDate": "2025-09-11T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4899863/submissions/265171869",
       "submitted": true
     },
     {
       "title": "HW09W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-11T23:59:00",
+      "dueDate": "2025-09-11T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4899864/submissions/265777608",
       "submitted": true
     },
     {
       "title": "HW10W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-18T23:59:00",
+      "dueDate": "2025-09-18T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4951694/submissions/267895908",
       "submitted": true
     },
     {
       "title": "HW12W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-18T23:59:00",
+      "dueDate": "2025-09-18T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/4951734/submissions/267895680",
       "submitted": true
     },
     {
       "title": "HW13W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-25T23:59:00",
+      "dueDate": "2025-09-25T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5002557/submissions/269768148",
       "submitted": true
     },
     {
       "title": "HW14W",
       "course": "MA.26600.116",
-      "dueDate": "2024-09-25T23:59:00",
+      "dueDate": "2025-09-25T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5002560/submissions/269768805",
       "submitted": true
     },
     {
       "title": "HW16W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-09T23:59:00",
+      "dueDate": "2025-10-09T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5040507/submissions/272411998",
       "submitted": true
     },
     {
       "title": "HW17W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-09T23:59:00",
+      "dueDate": "2025-10-09T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5040546/submissions/274471811",
       "submitted": true
     },
     {
       "title": "HW18W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-09T23:59:00",
+      "dueDate": "2025-10-09T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5040568/submissions/275031882",
       "submitted": true
     },
     {
       "title": "HW19W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-16T23:59:00",
+      "dueDate": "2025-10-16T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5138865/submissions/277102941",
       "submitted": true
     },
     {
       "title": "HW20W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-16T23:59:00",
+      "dueDate": "2025-10-16T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5138879/submissions/277103256",
       "submitted": true
     },
     {
       "title": "HW23W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-23T23:59:00",
+      "dueDate": "2025-10-23T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5183063/submissions/279565545",
       "submitted": true
     },
     {
       "title": "HW24W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-23T23:59:00",
+      "dueDate": "2025-10-23T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5183064/submissions/280330310",
       "submitted": true
     },
     {
       "title": "HW25W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-30T23:59:00",
+      "dueDate": "2025-10-30T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5220519/submissions/282185655",
       "submitted": true
     },
     {
       "title": "HW26W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-30T23:59:00",
+      "dueDate": "2025-10-30T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5220527/submissions/282185366",
       "submitted": true
     },
     {
       "title": "HW27W",
       "course": "MA.26600.116",
-      "dueDate": "2024-10-30T23:59:00",
+      "dueDate": "2025-10-30T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5220533/submissions/282890399",
       "submitted": true
     },
     {
       "title": "HW28W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5220544/submissions/283695762",
       "submitted": true
     },
     {
       "title": "HW29W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-08T23:59:00",
+      "dueDate": "2025-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5274554/submissions/286801861",
       "submitted": true
     },
     {
       "title": "HW30W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-08T23:59:00",
+      "dueDate": "2025-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5274571/submissions/286801700",
       "submitted": true
     },
     {
       "title": "HW31W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-08T23:59:00",
+      "dueDate": "2025-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5274578/submissions/286928865",
       "submitted": true
     },
     {
       "title": "HW32W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5324797/submissions/289584858",
       "submitted": true
     },
     {
       "title": "HW33W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5324798/submissions/289585220",
       "submitted": true
     },
     {
       "title": "HW34W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-22T23:59:00",
+      "dueDate": "2025-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5324799/submissions/292068348",
       "submitted": true
     },
     {
       "title": "HW35W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-22T23:59:00",
+      "dueDate": "2025-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5324802/submissions/290429825",
       "submitted": true
     },
     {
       "title": "HW36W",
       "course": "MA.26600.116",
-      "dueDate": "2024-11-22T23:59:00",
+      "dueDate": "2025-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826425/assignments/5324804/submissions/291315404",
       "submitted": true
     }
@@ -215,84 +215,84 @@ assignments = {
     {
       "title": "Homework 0",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-08-23T23:59:00",
+      "dueDate": "2025-08-23T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/4724310/submissions/261681036",
       "submitted": true
     },
     {
       "title": "Homework 1",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-08-30T23:59:00",
+      "dueDate": "2025-08-30T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/4801661/submissions/262763248",
       "submitted": true
     },
     {
       "title": "Homework 2",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-09-06T23:59:00",
+      "dueDate": "2025-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/4862555/submissions/264094537",
       "submitted": true
     },
     {
       "title": "Homework 3",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-09-13T23:59:00",
+      "dueDate": "2025-09-13T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/4913393/submissions/265302544",
       "submitted": true
     },
     {
       "title": "Homework 4",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-09-20T23:59:00",
+      "dueDate": "2025-09-20T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/4962395/submissions/268438009",
       "submitted": true
     },
     {
       "title": "Homework 5",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-10-04T23:59:00",
+      "dueDate": "2025-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5033155/submissions/274571529",
       "submitted": true
     },
     {
       "title": "Homework 6",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-10-13T23:59:00",
+      "dueDate": "2025-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5106604/submissions/277336486",
       "submitted": true
     },
     {
       "title": "Homework 7",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2025-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5148330/submissions/279120697",
       "submitted": true
     },
     {
       "title": "Homework 8",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-10-25T23:59:00",
+      "dueDate": "2025-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/821372",
       "submitted": false
     },
     {
       "title": "Homework 9",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-11-10T23:59:00",
+      "dueDate": "2025-11-10T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5224055/submissions/283371928",
       "submitted": true
     },
     {
       "title": "Homework 10",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-11-15T23:59:00",
+      "dueDate": "2025-11-15T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5296897/submissions/289353363",
       "submitted": true
     },
     {
       "title": "Homework 11",
       "course": "wl.202510.AAE.20300.002",
-      "dueDate": "2024-11-24T23:59:00",
+      "dueDate": "2025-11-24T23:59:00",
       "link": "https://www.gradescope.com/courses/821372/assignments/5333359/submissions/292063119",
       "submitted": true
     }
@@ -301,105 +301,105 @@ assignments = {
     {
       "title": "Homework 0",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-08-22T23:59:00",
+      "dueDate": "2025-08-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4750913/submissions/261906621",
       "submitted": true
     },
     {
       "title": "Homework 1",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-09-03T23:59:00",
+      "dueDate": "2025-09-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4809376/submissions/263649714",
       "submitted": true
     },
     {
       "title": "Homework 2",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-09-10T23:59:00",
+      "dueDate": "2025-09-10T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4880737/submissions/265240501",
       "submitted": true
     },
     {
       "title": "ISA1",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-09-19T23:59:00",
+      "dueDate": "2025-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4906099/submissions/267463537",
       "submitted": true
     },
     {
       "title": "Homework 3",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-09-19T23:59:00",
+      "dueDate": "2025-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/4949316/submissions/268414734",
       "submitted": true
     },
     {
       "title": "Homework 4",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-09-28T23:59:00",
+      "dueDate": "2025-09-28T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5001673/submissions/270976573",
       "submitted": true
     },
     {
       "title": "ISA2",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-10-10T23:59:00",
+      "dueDate": "2025-10-10T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5046730/submissions/276722781",
       "submitted": true
     },
     {
       "title": "Homework 5",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-10-03T23:59:00",
+      "dueDate": "2025-10-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5047452/submissions/274472816",
       "submitted": true
     },
     {
       "title": "ISA3",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-10-27T23:59:00",
+      "dueDate": "2025-10-27T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5152424/submissions/282429782",
       "submitted": true
     },
     {
       "title": "Homework 6",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-10-22T23:59:00",
+      "dueDate": "2025-10-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5156330/submissions/279917497",
       "submitted": true
     },
     {
       "title": "Homework 7",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-10-29T23:59:00",
+      "dueDate": "2025-10-29T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5210288/submissions/282532011",
       "submitted": true
     },
     {
       "title": "ISA4",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-11-16T23:59:00",
+      "dueDate": "2025-11-16T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5250046/submissions/289792281",
       "submitted": true
     },
     {
       "title": "Homework 8",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-11-07T23:59:00",
+      "dueDate": "2025-11-07T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5263008/submissions/286789084",
       "submitted": true
     },
     {
       "title": "Homework 9",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-11-19T23:59:00",
+      "dueDate": "2025-11-19T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5310062/submissions/290805396",
       "submitted": true
     },
     {
       "title": "ISA5",
       "course": "wl.202510.AAE.25100.002",
-      "dueDate": "2024-12-03T23:59:00",
+      "dueDate": "2025-12-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826063/assignments/5354985/submissions/294252576",
       "submitted": true
     }
