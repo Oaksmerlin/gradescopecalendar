18be4f3a20d59bac242b9e39e2fac472b9def945
planit <planit@tanjeffreyz.github.io>
2025-01-14 22:37:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,20 @@
 assignments = {
+  "ME-200 Division-7": [
+    {
+      "title": "HW-1",
+      "course": "ME-200 Division-7",
+      "dueDate": "2025-01-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/953819",
+      "submitted": false
+    },
+    {
+      "title": "HW-2",
+      "course": "ME-200 Division-7",
+      "dueDate": "2025-01-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/953819",
+      "submitted": false
+    }
+  ],
   "wl.202520.MA.30300.416": [
     {
       "title": "HW1",
