01d26f22cdd66a297ebf5176b54cea423fa83333
planit <planit@tanjeffreyz.github.io>
2025-02-08 05:36:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -69,6 +69,27 @@ assignments = {
       "dueDate": "2025-02-07T23:59:00",
       "link": "https://www.gradescope.com/courses/953819/assignments/5694956/submissions/305883361",
       "submitted": true
+    },
+    {
+      "title": "HW-11",
+      "course": "ME-200 Division-7",
+      "dueDate": "2025-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/953819",
+      "submitted": false
+    },
+    {
+      "title": "HW-12",
+      "course": "ME-200 Division-7",
+      "dueDate": "2025-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/953819",
+      "submitted": false
+    },
+    {
+      "title": "HW-13",
+      "course": "ME-200 Division-7",
+      "dueDate": "2025-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/953819",
+      "submitted": false
     }
   ],
   "wl.202520.AAE.20400.004": [
@@ -92,6 +113,13 @@ assignments = {
       "dueDate": "2025-02-07T23:59:00",
       "link": "https://www.gradescope.com/courses/954562/assignments/5743715/submissions/306366323",
       "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "wl.202520.AAE.20400.004",
+      "dueDate": "2025-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/954562",
+      "submitted": false
     }
   ],
   "wl.202520.AAE.20401.019": [
