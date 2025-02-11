b26c0d54947c57b666fc4d96475d0115dacd9d2e
planit <planit@tanjeffreyz.github.io>
2025-02-11 16:44:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -157,6 +157,13 @@ assignments = {
       "dueDate": "2025-02-19T23:59:00",
       "link": "https://www.gradescope.com/courses/955265",
       "submitted": false
+    },
+    {
+      "title": "Lab 2 Peer Review",
+      "course": "wl.202520.AAE.20401.019",
+      "dueDate": "2025-02-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/955265",
+      "submitted": false
     }
   ],
   "wl.202520.MA.30300.416": [
