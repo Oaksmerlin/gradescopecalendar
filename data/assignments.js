600763e90a2265b7c9242b2bc339f6124bbccaaf
planit <planit@tanjeffreyz.github.io>
2025-04-11 04:43:36 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -221,8 +221,8 @@ assignments = {
       "title": "HW-32",
       "course": "ME-200 Division-7",
       "dueDate": "2025-04-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/953819",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/953819/assignments/6036767/submissions/324384974",
+      "submitted": true
     }
   ],
   "wl.202520.AAE.20400.004": [
